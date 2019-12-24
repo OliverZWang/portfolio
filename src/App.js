423ee3153785047d697ef16e719c9a19984c5802
AnonymousUserOliver <zw1400@nyu.edu>
2019-12-23 23:41:18 -0500

    make header

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AppBar from './components/Header'; 
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
-  return (
+	return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+		<Router>
+			<AppBar></AppBar>
+			<Switch>
+				<Route exact path="/"/>
+			</Switch>
+		</Router>
+		
+      
     </div>
-  );
+    );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react'; 
+import {Link} from 'react-router-dom'; 
+import { withStyles } from '@material-ui/core/styles';
+import { AppBar } from '@material-ui/core';
+
+const styles = theme => ({
+    nav: {
+        background: "#1f1f1f",
+        // boxShadow: "none",
+        marginBottom: "15px",
+    },
+    container: {
+        textAlign: "center"
+    },
+    nav_item: {
+        listStyleType: "none",
+        float: "left",
+        margin: "30px",
+    },
+    nav_link: {
+        textTransform: "uppercase",
+        fontFamily: "Helvetica, Sans-Serif",
+        textDecoration: "none",
+        color: "white",
+        "&:hover": {
+            color: "grey"
+        }
+    },
+    wrapperUL: {
+        margin: "auto",
+        display: "inline-block",
+        padding: "0px"
+    }
+})
+
+
+
+class Header extends Component{
+    render(){
+        const {classes} = this.props; 
+        return(
+            <div>
+                <AppBar className={classes.nav} position='static'>
+                    <div className={classes.container}>
+                        <ul className={classes.wrapperUL}>
+                            <li className = {classes.nav_item} key={"About Me"}>
+                                <Link className={classes.nav_link} to={`/about-me`}>About Me</Link>
+                            </li>
+                            <li className = {classes.nav_item} key={"Education"}>
+                                <Link className={classes.nav_link} to={`/education`}>Education</Link>
+                            </li>
+                            <li className = {classes.nav_item} key={"Projects"}>
+                                <Link className={classes.nav_link} to={`/projects`}>Projects</Link>
+                            </li>
+                            <li className = {classes.nav_item} key={"Contact"}>
+                                <Link className={classes.nav_link} to={`/contact`}>Contact</Link>
+                            </li>
+                        </ul>
+
+                    </div>
+                </AppBar>
+
+            </div>
+        )
+    }
+}
+
+export default withStyles(styles)(Header)
